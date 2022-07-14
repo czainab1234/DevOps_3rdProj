f2719fc4c8871c73e46c2eb6a2a7c0f3a9a750c9
czainab1234 <100843695+czainab1234@users.noreply.github.com>
2022-07-14 14:41:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-scp -i ~/.ssh/id_rsa /var/lib/jenkins/workspace/devops-project/* ubuntu@18.133.195.54/opt
+scp -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -r /var/lib/jenkins/workspace/devops-project/* ubuntu@18.133.195.54:/opt
